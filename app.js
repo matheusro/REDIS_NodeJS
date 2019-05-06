dc770ce5b9f722b869ac1e27da0ac7fe1e465ff1
matheusro <matheusro@gmail.com>
2019-05-06 01:51:23 -0300

    First version of basic HTTP methods

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var redis = require('redis');
-var app = express();
-var client = redis.createClient();
 
-client.on('connect', function() {
+var app = express();
+var redisClient = redis.createClient();
+
+redisClient.on('connect', function() {
     console.log('Redis client connected');
 });
 
-client.on('error', function (err) {
+redisClient.on('error', function (err) {
     console.log('Something went wrong ' + err);
 });
 
 app.get('/', function (req, res) {
-    client.get('first_time_visitor', function (error, result) {
+
+    redisClient.get('first_time_visitor', function (error, result) {
         if (error) {
             console.log(error);
             throw error;
@@ -21,13 +23,52 @@ app.get('/', function (req, res) {
         console.log(result);
 
         if (result == null || result == 1) {
-            client.set('first_time_visitor', 0, redis.print);
+            redisClient.set('first_time_visitor', 450, redis.print);
             res.send('Hello new visitor!');
         }
         else{
             res.send('Hello visitor!');
         }
     });
+});
+
+app.post('/cart/:user_id', function(req, res){
+    var user_id = req.params.user_id
+    var cart_name = "cart_"+user_id
+    var empty_product = ""
+
+    redisClient.sadd(cart_name, empty_product)
+    console.log("New cart created " + cart_name)
+    res.sendStatus(200)
+});
+
+app.post('/cart/:user_id/products/:product_id', function(req, res){
+    var user_id = req.params.user_id
+    var cart_name = "cart_"+user_id
+    var product_id = req.params.product_id
+
+    redisClient.sadd(cart_name, product_id)
+    console.log("Product " + product_id + " added to cart " + cart_name)
+    res.sendStatus(200)
+});
+
+app.delete('/cart/:user_id', function(req, res){
+    var user_id = req.params.user_id
+    var cart_name = "cart_"+user_id
+
+    redisClient.del(cart_name)
+    console.log("Cart deleted " + cart_name)
+    res.sendStatus(200)
+});
+
+app.delete('/cart/:user_id/products/:product_id', function(req, res){
+    var user_id = req.params.user_id
+    var cart_name = "cart_"+user_id
+    var product_id = req.params.product_id
+
+    redisClient.srem(cart_name, product_id)
+    console.log("Product " + product_id + " removed from cart " + cart_name)
+    res.sendStatus(200)
 });
 
 app.listen(3000, function () {
