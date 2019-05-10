f1fb88f82f7b1fa855105a1e244761ead6bfc21b
Matheus Oliveira <matheusro@gmail.com>
2019-05-09 21:46:01 -0300

    Complete working version

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-const bodyParser = require('body-parser')
-
 var express = require('express');
 var redis = require('redis');
 
 var app = express();
 var redisClient = redis.createClient();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 redisClient.on('connect', function() {
     console.log('Redis client connected');
@@ -15,6 +10,26 @@ redisClient.on('connect', function() {
 
 redisClient.on('error', function (err) {
     console.log('Something went wrong ' + err);
+});
+
+app.get('/', function (req, res) {
+
+    redisClient.get('first_time_visitor', function (error, result) {
+        if (error) {
+            console.log(error);
+            throw error;
+        }
+
+        console.log(result);
+
+        if (result == null || result == 1) {
+            redisClient.set('first_time_visitor', 450, redis.print);
+            res.send('Hello new visitor!');
+        }
+        else{
+            res.send('Hello visitor!');
+        }
+    });
 });
 
 app.post('/cart/:user_id', function(req, res){
@@ -27,10 +42,10 @@ app.post('/cart/:user_id', function(req, res){
     res.sendStatus(200)
 });
 
-app.post('/cart/:user_id/products/', function(req, res){
+app.post('/cart/:user_id/products/:product_id', function(req, res){
     var user_id = req.params.user_id
     var cart_name = "cart_"+user_id
-    var product_id = req.body.product_id
+    var product_id = req.params.product_id
 
     redisClient.sadd(cart_name, product_id)
     console.log("Product " + product_id + " added to cart " + cart_name)
@@ -54,23 +69,6 @@ app.delete('/cart/:user_id/products/:product_id', function(req, res){
     redisClient.srem(cart_name, product_id)
     console.log("Product " + product_id + " removed from cart " + cart_name)
     res.sendStatus(200)
-});
-
-app.get('/cart/:user_id', function(req, res){
-    var user_id = req.params.user_id
-    var cart_name = "cart_"+user_id
-
-    redisClient.smembers(cart_name, function(err, products){
-        console.log("Products in cart: " + products);
-        // //var strProdutos = products + "".;
-        // var listaProdutos = products.split(",");
-
-        // for(i=0; i < listaProdutos.length; i++){
-        //     res.write(listaProdutos[i]+"<BR>");
-        // }
-        res.write(""+products);
-        res.status(200).end();
-      });
 });
 
 app.listen(3000, function () {
